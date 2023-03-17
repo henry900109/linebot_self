3149491bcac711e327fb5fe118e2f8a5ef27d480
henry <henry2.900109@gmail.com>
2023-03-17 19:25:42 +0800

    W

--- api/index.py
+++ api/index.py
@@ -51,7 +51,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="好的，我會安靜"))
-    elif message == "!回來" or "!hello":
+    elif message == "!回來" or message == "!hello":
         quiet_mode = False
         line_bot_api.reply_message(
             event.reply_token,
