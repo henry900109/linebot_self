50bef168ecc1b863e5484b793d3c92921ea1304a
henry <henry2.900109@gmail.com>
2023-03-17 19:36:37 +0800

    W1

--- api/index.py
+++ api/index.py
@@ -63,8 +63,9 @@ def handle_message(event):
                 line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=retext))
-            elif message[0] == "!" and "市天氣" == message[3:]:
+            elif message[0] == "!" and "天氣" == message[4:]:
         # 用氣象局 API 查詢板橋區的天氣資訊
+                country = ["宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣", "臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市", "基隆市", "新竹縣", "新竹市", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義縣", "嘉義市", "屏東縣"]
                 LOCATION_NAME = message[1:4]
                 res = requests.get(f"{WEATHER_API_URL}?Authorization={WEATHER_API_KEY}&locationName={LOCATION_NAME}")
                 if res.status_code == 200:
