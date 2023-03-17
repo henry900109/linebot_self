b8196d1cf2c2af734860da18f5626bb30c6b8f33
henry <henry2.900109@gmail.com>
2023-03-17 14:06:05 +0800

    3

--- api/index.py
+++ api/index.py
@@ -41,8 +41,6 @@ def handle_message(event):
     
 
     if working_status:
-        @line_handler.add(MessageEvent, message=TextMessage)
-        def handle_message(event):
           line_bot_api.reply_message(
           event.reply_token,
             TextSendMessage(text=event.message.text))
