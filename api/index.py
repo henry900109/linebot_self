39b049ca8ffbc79c6cdee1c1b0d833592111f7ca
henry <henry2.900109@gmail.com>
2023-03-17 15:25:48 +0800

    test

--- api/index.py
+++ api/index.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # domain root
 @app.route('/')
 def home():
-    return 'Hello'
+    return line_bot_api,line_handler
 
 @app.route("/webhook", methods=['POST'])
 def callback():
