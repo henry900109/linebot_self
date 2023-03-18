6e157a30bbe8f06e070971ed5066988a38219a06
henry <henry2.900109@gmail.com>
2023-03-18 12:26:59 +0800

    tt2

--- test/test.py
+++ test/test.py
@@ -3,6 +3,6 @@ def test():
 def profile(profile):
     name = profile.display_name
     id = profile.user_id
-    pic = profile.picture_url
+    # pic = profile.picture_url
     m = profile.status_message
-    return name +"\n"+ id +"\n" + pic +"\n" + m
+    return name +"\n"+ id +"\n" + m
