98175209f65dace4acfb0062feeadb4437bf7251
henry <henry2.900109@gmail.com>
2023-03-18 21:20:51 +0800

    tt6

--- utils/jsonfile.py
+++ utils/jsonfile.py
@@ -1,14 +1,16 @@
 import json
 import os
-current_path = os.path.dirname(__file__)
+
 
 def checkfile(id,username):
-    with open(r'\var\task\docs\user.json') as f:
-        data = json.load(f)
-    if id in data:
-        return True
-    else:
-        return False
-        data[id] = username
-        with open(r'..\docs\user.json', 'w') as f:
-            json.dump(data, f,ensure_ascii=False)
+    current_path = os.path.dirname(__file__)
+    return current_path
+    # with open(r'\var\task\docs\user.json') as f:
+    #     data = json.load(f)
+    # if id in data:
+    #     return True
+    # else:
+    #     return False
+    #     data[id] = username
+    #     with open(r'..\docs\user.json', 'w') as f:
+    #         json.dump(data, f,ensure_ascii=False)
