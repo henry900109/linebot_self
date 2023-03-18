e1ff16e0d1fbc99823b451f73b9d42b019f2ad6c
henry <henry2.900109@gmail.com>
2023-03-18 17:05:49 +0800

    tt4

--- test/test.py
+++ test/test.py
@@ -2,13 +2,13 @@ import json
 def test():
     return "hello world"
 def profile(profile):
-    name = profile.display_name
-    # id = profile.user_id
+    # name = profile.display_name
+    id = profile.user_id
     # pic = profile.picture_url
     #m = profile.status_message
         #     'profile_name': profile.display_name,
         # 'profile_id': profile.user_id,
-    return name 
+    return id
 def group(profile):
     id = profile.group_id  # 群組 ID
     name = profile.group_name  # 群組名稱
