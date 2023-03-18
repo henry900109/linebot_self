c0d763b9c9ef5cbbd9b296ed5136fc4de821a767
henry <henry2.900109@gmail.com>
2023-03-18 22:33:08 +0800

    Gmode1

--- Game/Guessnumber.py
+++ Game/Guessnumber.py
@@ -10,16 +10,16 @@ def Guessnumber(user_input,range_min,range_max,answer):
         user_number = int(user_input)
 
         if user_number < range_min or user_number > range_max:
-            return "請輸入範圍內的數字！"
+            return "請輸入範圍內的數字！" + answer
         
         if user_number == answer:
-            return "恭喜你，猜中了！"
+            return "恭喜你，猜中了！" + answer
         
         elif user_number < answer:
-            return "太小了，再猜一次！"
+            return "太小了，再猜一次！" + answer
         
         else:
-            return "太大了，再猜一次！"
+            return "太大了，再猜一次！" + answer
         
     except ValueError:
         return "請輸入一個數字！"
