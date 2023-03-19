4cde7b372c49b8dee857e3a6bac86defcf7ec0cc
henry <henry2.900109@gmail.com>
2023-03-19 11:25:28 +0800

    C2

--- utils/gpt.py
+++ utils/gpt.py
@@ -3,10 +3,18 @@ import openai
 def gpt3_5(token,message):
     openai.api_key = token
     respone = openai.ChatCompletion.create(
+
     model="gpt-3.5-turbo",
 
-    messages=[
-            {"role": "user", "content": message},
-    ]
+    messages = [ {"role": "user", "content": message}]
+
     )
-    return respone.choices[0].message.content
+
+    respone = respone.choices[0].message.content
+
+    respone = respone.replace("\n","",True) if "\n" in respone[10] else respone
+
+    return respone
+
+if __name__ == '__main__':
+    print(gpt3_5(token="",message="你是誰?"))
