3bcd7e10c52336a6a5e76f63974db3f05202ac3f
henry <henry2.900109@gmail.com>
2023-03-21 18:10:14 +0800

    WW3

--- utils/weather.py
+++ utils/weather.py
@@ -104,12 +104,12 @@ def weather(WEATHER_API_KEY,user):
 
                 relpy_text+='N\A'
 
-        if relpy_text == user[2:] +"\n"+'\n':
+    if relpy_text == user[2:] +"\n"+'\n':
 
-            relpy_text = "天氣查詢失敗"
+        relpy_text = "天氣查詢失敗"
 
-        else:
-            relpy_text = relpy_text[:-2]
+    else:
+        relpy_text = relpy_text[:-2]
 
     return relpy_text
     
