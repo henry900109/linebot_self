bbb53aed43ad602ab811644a52257696dc25b717
henry <henry2.900109@gmail.com>
2023-05-02 11:44:34 +0800

    gpt3V6

--- utils/gpt.py
+++ utils/gpt.py
@@ -11,14 +11,15 @@ def gpt3_5(token, message):
                 temperature=0.9,
                 frequency_penalty=0,
                 #預設的 OPENAI_FREQUENCY_PENALTY 為 0.5，OPENAI_PRESENCE_PENALTY 為 0.0
-                presence_penalty=0.6
+                presence_penalty=0.6,
+                stop=[" Human:", " AI:"]
                 #
                 )
             # 接收到回覆訊息後，移除換行符號
 
   reply_msg = response["choices"][0]["text"].replace('AI:','')
   reply_msg = reply_msg.strip()
-  
+
   return reply_msg
 
 if __name__ == '__main__':
