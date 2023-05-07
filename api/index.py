7df94237b3d6397184cd4ad41b39eb707f539ac3
henry900109 <henry2.900109@gmail.com>
2023-05-07 13:53:10 +0800

    gpt3VX5

--- api/index.py
+++ api/index.py
@@ -152,7 +152,6 @@ def handle_message(event):
                 if gpttemplate != "":
                     gpttemplate = gpttemplate + message
                 else:
-                    reply_text = ug.gpt3_5(Openai_token,template + '你好')
                     gpttemplate = template + message
                 try:
                     reply_text = ug.gpt3_5(Openai_token,gpttemplate)
