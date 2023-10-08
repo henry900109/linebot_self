7fc3fb435672d75f4fb0aa137d8a287ed88027bc
henry900109 <henry2.900109@gmail.com>
2023-10-08 22:40:46 +0800

    youtube3

--- utils/dollar.py
+++ utils/dollar.py
@@ -6,7 +6,7 @@ def UAH2TWD():
     r=requests.get("https://tw.rter.info/capi.php")
     currency=r.json()
     dollar = (149/currency["USDUAH"]['Exrate'])*currency["USDTWD"]['Exrate']
-    callback = "烏克蘭 Youtube Premium\n" +currency["USDUAH"]['UTC'].split()[0].split("-")[0] + "年"+ currency["USDUAH"]['UTC'].split()[0].split("-")[1] + "月\n新臺幣: " + str(dollar) + "元\n6人均分每人: " + str(dollar/6) + "元\n\n更新時間: \n" + currency["USDUAH"]['UTC'] + " 和 " + currency["USDTWD"]['UTC']
+    callback = "烏克蘭 Youtube Premium\n" +currency["USDUAH"]['UTC'].split()[0].split("-")[0] + "年"+ currency["USDUAH"]['UTC'].split()[0].split("-")[1] + "月\n新臺幣: " + str(dollar) + "元\n6人均分每人: " + str(dollar/6) + "元\n\n更新時間: \n" + currency["USDUAH"]['UTC'].split()[0] + " 和 " + currency["USDTWD"]['UTC'].split()[0]
     return callback
 if __name__ == '__main__':
     print(UAH2TWD())
