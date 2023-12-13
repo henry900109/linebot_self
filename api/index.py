9989927c826439910f7b954717c74b1df8804b9a
henry900109 <58168575+henry900109@users.noreply.github.com>
2023-12-13 20:25:08 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -232,6 +232,10 @@ def handle_message(event):
                     messageA,messageB  = message.split(" ")[0],message.split(" ")[1]
                     reply_text = pokemon.Attributes_Table(messageB,messageA)
                     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text))
+                elif "!pokename" in message:
+                    message = message[9:]
+                    reply_text = pokemon.attr(message)
+                    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text))
                 else:
 
                     #確認是否在遊戲模式
